98c5b5a7e90505fa1b96abfde9b599fda59537da
JackLuson <18219112108@163.com>
2018-11-12 21:49:05 +0800

    增加导航栏动效

---
+++ assets/js/Luxm.js
@@ -0,0 +1,27 @@
+// Navigation Scripts to Show Header on Scroll-Up
+jQuery(document).ready(function ($) {
+  //primary navigation slide-in effect
+  console.log('window.width', $(window).height())
+  var headerHeight = $('.masthead').height(),
+    bannerHeight = $('.intro-header .container').height();
+  $(window).on('scroll',
+    function (event) {
+      // console.log('eventdata', event.data.previousTop)
+      var currentTop = $(window).scrollTop();
+
+      //check if user is scrolling up by mouse or keyborad
+      if (currentTop < this.previousTop) {
+        //if scrolling up...
+        if (currentTop > 0 && $('.masthead').hasClass('is-fixed')) {
+          $('.masthead').addClass('is-visible');
+        } else {
+          $('.masthead').removeClass('is-visible is-fixed');
+        }
+      } else {
+        //if scrolling down...
+        $('.masthead').removeClass('is-visible');
+        if (currentTop > headerHeight && !$('.masthead').hasClass('is-fixed')) $('.masthead').addClass('is-fixed');
+      }
+      this.previousTop = currentTop;
+    });
+});
